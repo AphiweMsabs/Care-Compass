915312d5b383368f5b9b97da00915ea78e308c6f
nsibiya023 <nsibiya023@student.wethinkcode.co.za>
2024-07-16 17:37:15 +0200

    Sign up page start up

--- Website/home_page.py
+++ Website/home_page.py
@@ -5,4 +5,4 @@ registerUser= Blueprint("register",__name__)
 @registerUser.route('/sign-up',methods=['GET', 'POST'])
 
 def signUp():
-     return render_template("index.html")
+     return render_template("signUpPage.html")
